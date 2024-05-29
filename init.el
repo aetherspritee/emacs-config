a192187a19105da97b0cc4d4f3107de935a9f8f8
aetherspritee <schautendustin@gmail.com>
2024-05-29 21:05:26 +0200

    go to refs keybind

--- init.el
+++ init.el
@@ -177,40 +177,43 @@ If on a:
     ;; (define-key org-mode-map (kbd "RET") #'+org/dwim-at-point)
 
 (use-package evil
-      :init ;; Execute code Before a package is loaded
-      (evil-mode)
-      :config ;; Execute code After a package is loaded
-      (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
-      :custom ;; Customization of package custom variables
-      (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
-      (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
-      (evil-want-C-i-jump nil)      ;; Disables C-i jump
-      (evil-undo-system 'undo-redo) ;; C-r to redo
-      (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
-      ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
-      :bind (:map evil-motion-state-map
-                  ("SPC" . nil)
-                  ("RET" . nil)
-                  ;; ("RET" . org-todo)
-                  ("TAB" . nil)))
-    (use-package evil-collection
-      :after evil
-      :config
-      ;; Setting where to use evil-collection
-      ;; (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult lsp-ui-imenu))
-      (evil-set-initial-state 'package-menu-mode 'motion)
-      (evil-collection-init))
-    (use-package evil-commentary
-      :after evil
-      :config
-      (evil-commentary-mode)
-      )
-(with-eval-after-load 'evil-maps
-  (define-key evil-motion-state-map (kbd "SPC") '+org/dwim-at-point)
-  (define-key evil-motion-state-map (kbd "RET") nil)
-  (define-key evil-motion-state-map (kbd "TAB") nil))
-;; Setting RETURN key in org-mode to follow links
-  (setq org-return-follows-link  t)
+          :init ;; Execute code Before a package is loaded
+          (evil-mode)
+          :config ;; Execute code After a package is loaded
+          (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
+          :custom ;; Customization of package custom variables
+          (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
+          (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
+          (evil-want-C-i-jump nil)      ;; Disables C-i jump
+          (evil-undo-system 'undo-redo) ;; C-r to redo
+          (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
+          ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
+          :bind (:map evil-motion-state-map
+                      ("SPC" . nil)
+                      ("RET" . nil)
+                      ;; ("RET" . org-todo)
+                      ("TAB" . nil)))
+        (use-package evil-collection
+          :after evil
+          :config
+          ;; Setting where to use evil-collection
+          ;; (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult lsp-ui-imenu))
+          (evil-set-initial-state 'package-menu-mode 'motion)
+          (evil-collection-init)
+        (setq evil-collection-want-find-usages-bindings t)
+)
+        (use-package evil-commentary
+          :after evil
+          :config
+          (evil-commentary-mode)
+          )
+    (with-eval-after-load 'evil-maps
+      (define-key evil-motion-state-map (kbd "SPC") '+org/dwim-at-point)
+      (define-key evil-motion-state-map (kbd "RET") nil)
+      (define-key evil-motion-state-map (kbd "TAB") nil)
+      (define-key evil-motion-state-map (kbd "g r") 'lsp-find-references))
+    ;; Setting RETURN key in org-mode to follow links
+      (setq org-return-follows-link  t)
 
 (use-package general
           :config
@@ -240,12 +243,15 @@ If on a:
             "H" '(enlight-open :wk "show dashboard"))
 
           (start/leader-keys
-            "o a" '(org-agenda :wk "Open agenda"))
+            "o a" '(org-agenda :wk "Open agenda")
+            "o n" '(treemacs :wk "Treemacs")
+            )
 
         (start/leader-keys
             "c c" '(compile :wk "compile")
             "c k" '(kill-compilation :wk "kill compilation")
-            "c C" '(recompile :wk "kill compilation"))
+            "c C" '(recompile :wk "kill compilation")
+            "c s" '(lsp-treemacs-symbols :wk "treemacs symbols"))
 
           (start/leader-keys
             "TAB n" '(+workspace/swap-right :wk "Next workspace")
@@ -330,7 +336,9 @@ If on a:
             "t t" '(visual-line-mode :wk "Toggle truncated lines (wrap)")
             "t l" '(display-line-numbers-mode :wk "Toggle line numbers")))
 
-
+    ;; (add-hook prog-mode-hook
+    ;; (lambda ()
+    ;; (local-set-key "g r" 'lsp-find-references)))
 
     (add-hook 'org-mode-hook (lambda ()
            (setq-local electric-pair-inhibit-predicate
@@ -1875,7 +1883,7 @@ This be hooked to `projectile-after-switch-project-hook'."
   (when-let ((persp-tab-data (persp-parameter 'tab-bar-tabs)))
     (tab-bar-tabs-set persp-tab-data)
     (tab-bar--update-tab-bar-lines t)))
-
+(setq persp-suppress-no-prefix-key-warning t)
 ;;
 ;;; Advice
 
@@ -2134,3 +2142,121 @@ stored in `persp-save-dir'.")
       (add-hook 'persp-before-save-state-to-file-functions #'+workspaces-save-tab-bar-data-to-file-h)
       (+workspaces-load-tab-bar-data-from-file-h))))
 (add-hook 'after-init-hook (lambda () (persp-mode)))
+
+(use-package treemacs
+      :ensure t
+      :defer t
+      :init
+      (with-eval-after-load 'winum
+        (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+      :config
+      (progn
+        (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+              treemacs-deferred-git-apply-delay        0.5
+              treemacs-directory-name-transformer      #'identity
+              treemacs-display-in-side-window          t
+              treemacs-eldoc-display                   'simple
+              treemacs-file-event-delay                2000
+              treemacs-file-extension-regex            treemacs-last-period-regex-value
+              treemacs-file-follow-delay               0.2
+              treemacs-file-name-transformer           #'identity
+              treemacs-follow-after-init               t
+              treemacs-expand-after-init               t
+              treemacs-find-workspace-method           'find-for-file-or-pick-first
+              treemacs-git-command-pipe                ""
+              treemacs-goto-tag-strategy               'refetch-index
+              treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+              treemacs-hide-dot-git-directory          t
+              treemacs-indentation                     2
+              treemacs-indentation-string              " "
+              treemacs-is-never-other-window           nil
+              treemacs-max-git-entries                 5000
+              treemacs-missing-project-action          'ask
+              treemacs-move-files-by-mouse-dragging    t
+              treemacs-move-forward-on-expand          nil
+              treemacs-no-png-images                   nil
+              treemacs-no-delete-other-windows         t
+              treemacs-project-follow-cleanup          nil
+              treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+              treemacs-position                        'left
+              treemacs-read-string-input               'from-child-frame
+              treemacs-recenter-distance               0.1
+              treemacs-recenter-after-file-follow      nil
+              treemacs-recenter-after-tag-follow       nil
+              treemacs-recenter-after-project-jump     'always
+              treemacs-recenter-after-project-expand   'on-distance
+              treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+              treemacs-project-follow-into-home        nil
+              treemacs-show-cursor                     nil
+              treemacs-show-hidden-files               t
+              treemacs-silent-filewatch                nil
+              treemacs-silent-refresh                  nil
+              treemacs-sorting                         'alphabetic-asc
+              treemacs-select-when-already-in-treemacs 'move-back
+              treemacs-space-between-root-nodes        t
+              treemacs-tag-follow-cleanup              t
+              treemacs-tag-follow-delay                1.5
+              treemacs-text-scale                      nil
+              treemacs-user-mode-line-format           nil
+              treemacs-user-header-line-format         nil
+              treemacs-wide-toggle-width               70
+              treemacs-width                           35
+              treemacs-width-increment                 1
+              treemacs-width-is-initially-locked       t
+              treemacs-workspace-switch-cleanup        nil)
+
+        ;; The default width and height of the icons is 22 pixels. If you are
+        ;; using a Hi-DPI display, uncomment this to double the icon size.
+        ;;(treemacs-resize-icons 44)
+
+        (treemacs-follow-mode t)
+        (treemacs-filewatch-mode t)
+        (treemacs-fringe-indicator-mode 'always)
+        (when treemacs-python-executable
+          (treemacs-git-commit-diff-mode t))
+
+        (pcase (cons (not (null (executable-find "git")))
+                     (not (null treemacs-python-executable)))
+          (`(t . t)
+           (treemacs-git-mode 'deferred))
+          (`(t . _)
+           (treemacs-git-mode 'simple)))
+
+        (treemacs-hide-gitignored-files-mode nil))
+      :bind
+      (:map global-map
+            ("M-0"       . treemacs-select-window)
+            ("C-x t 1"   . treemacs-delete-other-windows)
+            ("C-x t t"   . treemacs)
+            ("C-x t d"   . treemacs-select-directory)
+            ("C-x t B"   . treemacs-bookmark)
+            ("C-x t C-t" . treemacs-find-file)
+            ("C-x t M-t" . treemacs-find-tag)))
+
+    (use-package treemacs-evil
+      :after (treemacs evil)
+      :ensure t)
+
+    ;; (use-package treemacs-projectile
+    ;;   :after (treemacs projectile)
+    ;;   :ensure t)
+
+    ;; (use-package treemacs-icons-dired
+    ;;   :hook (dired-mode . treemacs-icons-dired-enable-once)
+    ;;   :ensure t)
+
+    (use-package treemacs-magit
+      :after (treemacs magit)
+      :ensure t)
+
+    ;; (use-package treemacs-persp 
+    ;;   :after (treemacs persp-mode) 
+    ;;   :ensure t
+    ;;   :config (treemacs-set-scope-type 'Perspectives))
+
+    ;; (use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
+    ;;   :after (treemacs)
+    ;;   :ensure t
+    ;;   :config (treemacs-set-scope-type 'Tabs))
+
+(lsp-treemacs-sync-mode 1)
